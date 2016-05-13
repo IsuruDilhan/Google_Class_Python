7a699e64e538ef438fe138264642d9d03dcff188
Isuru <isuru.dilhan@yahoo.com>
2016-05-13 16:26:12 +0530

    fixes

--- google-python-exercises/hello.py
+++ google-python-exercises/hello.py
@@ -16,18 +16,17 @@ Try changing the 'Hello' to 'Howdy' and run again.
 Once you have that working, you're ready for class -- you can edit
 and run Python code; now you just need to learn Python!
 """
+import Tkinter
+import tkMessageBox
 
-import sys
+top = Tkinter.Tk()
+# Code to add widgets will go here...
 
-# Define a main() function that prints a little greeting.
-def main():
-  # Get the name from the command line, using 'World' as a fallback.
-  if len(sys.argv) >= 2:
-    name = sys.argv[1]
-  else:
-    name = 'World'
-  print 'Hello', name
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
 
-# This is the standard boilerplate that calls the main() function.
-if __name__ == '__main__':
-  main()
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+
+top.mainloop()
